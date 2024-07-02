88332bfc757975a4dd5bdba29d5e1eef760399d5
RaidenXVR <fitran.nizar@gmail.com>
2024-07-02 11:16:36 +0700

    connected Menu with Foods in Database

--- src/main/java/com/keldelapan/CafeMenuManagement/controller/HomeController.java
+++ src/main/java/com/keldelapan/CafeMenuManagement/controller/HomeController.java
@@ -1,14 +1,20 @@
 package com.keldelapan.CafeMenuManagement.controller;
 
 
+import com.keldelapan.CafeMenuManagement.service.FoodService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private FoodService foodService;
     @GetMapping("/home")
     public String home(Model model){
+
+        model.addAttribute("foods", foodService.getAllFoods());
         return "home";
     }
 }
